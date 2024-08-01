5ca6aadb55188e32f31fbeb485af75a083a7cf1c
Banti Sharma <vkv851203@gmail.co>
2024-08-01 17:04:48 +0530

    everything fixed

---
+++ src/Components/Main/Data.js
@@ -0,0 +1,60 @@
+import img from "../Assets/image(Travel).jpg";
+import img1 from "../Assets/image(Travel1).jpg";
+import img2 from "../Assets/Image(Travel2).jpg";
+
+export const Data=[
+    {
+        id:1,
+        imageSrc:img,
+        descTitle:'India Gate',
+        location:'Mumbai, India',
+        grade:'CULTURAL RELAX',
+        fees:'$700',
+        description:'The India Gate (formerly known as All India War Memorial) is a war memorial located near the Kartavya path on the eastern edge of the "ceremonial axis" of New Delhi, formerly called Rajpath in New Delhi. It stands as a memorial to 74,187 soldiers of the Indian Army who died between 1914 and 1921 in the First World War, in France, Flanders, Mesopotamia, Persia, East Africa, Gallipoli and elsewhere in the Near and the Far East, and the Third Anglo-Afghan War.'
+    },
+    {
+        id:2,
+        imageSrc:img1,
+        descTitle:'Sikkim',
+        location:'Sikkim, India',
+        grade:'CULTURAL RELAX',
+        fees:'$800',
+        description:'The Kingdom of Sikkim was founded by the Namgyal dynasty in the 17th century. It was ruled by Buddhist priest-kings known as the Chogyal. It became a princely state of the British Indian Empire in 1890.'
+    },
+    {
+        id:3,
+        imageSrc:img2,
+        descTitle:'Cappadocia',
+        location:'Peeru',
+        grade:'CULTURAL RELAX',
+        fees:'$800',
+        description:'The epitome of romance,Bora Bora is one of the best travel destinations in the world.This place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id:4,
+        imageSrc:img2,
+        descTitle:'Cappadocia',
+        location:'Peeru',
+        grade:'CULTURAL RELAX',
+        fees:'$800',
+        description:'The epitome of romance,Bora Bora is one of the best travel destinations in the world.This place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id:5,
+        imageSrc:img2,
+        descTitle:'Cappadocia',
+        location:'Peeru',
+        grade:'CULTURAL RELAX',
+        fees:'$800',
+        description:'The epitome of romance,Bora Bora is one of the best travel destinations in the world.This place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id:6,
+        imageSrc:img,
+        descTitle:'India Gate',
+        location:'Mumbai, India',
+        grade:'CULTURAL RELAX',
+        fees:'$800',
+        description:'The India Gate (formerly known as All India War Memorial) is a war memorial located near the Kartavya path on the eastern edge of the "ceremonial axis" of New Delhi, formerly called Rajpath in New Delhi. It stands as a memorial to 74,187 soldiers of the Indian Army who died between 1914 and 1921 in the First World War, in France, Flanders, Mesopotamia, Persia, East Africa, Gallipoli and elsewhere in the Near and the Far East, and the Third Anglo-Afghan War.'
+    },
+]
